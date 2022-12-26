6525cf44374c830f02abc789388fa7738e5bce08
Jules Ferlin <jules.ferlin@gmail.com>
2022-12-26 22:30:40 +0100

    Minor changed

--- delete_command.js
+++ delete_command.js
@@ -5,15 +5,6 @@ dotenv.config();
 
 const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
-// ...
-/*
-// for guild-based commands
-rest.put(Routes.applicationGuildCommands("1032608939013050389", "1031963275556048906"), { body: [] })
-  .then(() => console.log('Successfully deleted all guild commands.'))
-  .catch(console.error);
-
- */
-
 // for global commands
 rest.put(Routes.applicationCommands(process.env.clientId), { body: [] })
   .then(() => console.log('Successfully deleted all application commands.'))
