b3dee35a62107b2b3c9317acc99ac372de2f8684
Jules Ferlin <jules.ferlin@gmail.com>
2022-12-28 17:19:59 +0100

    Put that files in comment waiting for changes

--- commands/stats.js
+++ commands/stats.js
@@ -1,26 +1,33 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('stats')
-        .setDescription('Mets a jour les statisques du serveur')
-        .setDefaultMemberPermissions(0),
-    async execute(interaction) {
-        await interaction.reply({content:`Server Stats is updating...`, ephemeral: true});
-        let channel = interaction.guild.channels.cache.get(c_cotisant);
-        let nb = interaction.guild.roles.cache.get(r_cotisant).members;
-        nb = nb.map(m => m.displayName);
-        nb = nb.length;
-        channel.setName(`🏃 Cotisants : ${nb}`);
+  data: new SlashCommandBuilder()
+    .setName('stats')
+    .setDescription('Mets a jour les statisques du serveur')
+    .setDefaultMemberPermissions(0),
+  async execute(interaction) {
+    console.log("to do");
+    await interaction.reply({
+      content: "to do",
+      ephemeral: true,
+    })
+    /*
+    await interaction.reply({content:`Server Stats is updating...`, ephemeral: true});
+    let channel = interaction.guild.channels.cache.get(c_cotisant);
+    let nb = interaction.guild.roles.cache.get(r_cotisant).members;
+    nb = nb.map(m => m.displayName);
+    nb = nb.length;
+    channel.setName(`🏃 Cotisants : ${nb}`);
 
-        channel = interaction.guild.channels.cache.get(c_nonCotisant);
-        nb = interaction.guild.roles.cache.get(r_nonCotisant).members;
-        nb = nb.map(m => m.displayName);
-        nb = nb.length;
-        channel.setName(`🦽 Non Cotisants : ${nb}`);
+    channel = interaction.guild.channels.cache.get(c_nonCotisant);
+    nb = interaction.guild.roles.cache.get(r_nonCotisant).members;
+    nb = nb.map(m => m.displayName);
+    nb = nb.length;
+    channel.setName(`🦽 Non Cotisants : ${nb}`);
 
-        channel = interaction.guild.channels.cache.get(c_total);
-        let total = interaction.guild.memberCount;
-        channel.setName(`🌍 Total : ${total}`);
-    },
+    channel = interaction.guild.channels.cache.get(c_total);
+    let total = interaction.guild.memberCount;
+    channel.setName(`🌍 Total : ${total}`);
+    */
+  },
 };
