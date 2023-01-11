7049c7c4e76dea57bc5731de62f927e618e8390e
Jules Ferlin <jules.ferlin@gmail.com>
2023-01-11 12:53:31 +0100

    Change async -> await function and cached the rule message

--- events/ready.js
+++ events/ready.js
@@ -6,15 +6,23 @@ const { rolesFiles } = require("../utils/roles_files");
 module.exports = {
   name: Events.ClientReady,
   once: true,
-  execute(client) {
-    client.user.setUsername('BDS');
-    client.user.setAvatar('./logo/logo1.png');
-    client.user.setActivity("JJG's songs", { type: ActivityType.Listening });
-    client.user.setPresence({ status: 'dnd', });
+  async execute(client) {
+    await client.user.setUsername('BDS');
+    await client.user.setAvatar('./logo/logo1.png');
+    await client.user.setActivity("JJG's songs", {type: ActivityType.Listening});
+    client.user.setPresence({status: 'dnd',});
     let guild = client.guilds.cache.map(m => m);
     deleteOldestFiles();
     rolesFiles(guild);
     channelFiles(guild);
     console.log(`Ready! Logged in as ${client.user.tag}`);
+
+    const g = await client.guilds.fetch('1040600799434260502');
+    const channel = g.channels.cache.get('1061602590938382376');
+
+    let message;
+    if (channel.isTextBased()) {
+      message = await channel.messages.fetch(channel.messages.lastMessageID);
+    }
   },
 };
