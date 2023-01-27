ada30f2a8bb9be03a6330c5c6f5a7b93e4a3b651
Jules Ferlin <jules.ferlin@gmail.com>
2023-01-27 19:37:03 +0100

    removed old stuff

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,8 +3,7 @@ const { Events } = require('discord.js');
 module.exports = {
   name: Events.GuildMemberAdd,
   async execute(member) {
-    const { arrivée } = require(`./serveur/channels/channels_${member.guild.id}.json`);
-    //const { Attente_Cotisant, Change_de_nom } = require(`./serveur/roles/role_${member.guild.id}.json`);
+    const { arrivée } = require(`../serveur/channels/channels_${member.guild.id}.json`);
 
     console.log(`${member.user.tag} a rejoins le serveur ${member.guild.name}`);
 
@@ -15,19 +14,5 @@ module.exports = {
       let channel = member.guild.channels.cache.get(arrivée);
       channel.send(`${member.user.tag} joined`);
     }
-
-    /*
-    // Mise en place des rôles
-    if (Attente_Cotisant === undefined || Change_de_nom === undefined) {
-      console.log("Aucun Role");
-    } else {
-      let tmp = member.displayName.split(' ');
-      if (tmp.length <= 1) {
-          member.roles.add(Change_de_nom);
-      } else {
-          member.roles.add(Attente_Cotisant);
-      }
-    }
-    */
   },
 };
