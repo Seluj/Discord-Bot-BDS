88d7d44b20e371f9e132a2cfd570a727226f7fc7
Jules Ferlin <jules.ferlin@gmail.com>
2023-01-31 00:21:43 +0100

    improved ping answer

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,6 @@ module.exports = {
     .setName('ping')
     .setDescription('Répond avec Pong!'),
   async execute(interaction) {
-    await interaction.reply({content: 'Pong!', ephemeral: true});
+    await interaction.reply({content: `\n> 🏓La latence est de ${Date.now() - interaction.createdTimestamp} ms.\n> La latence avec l'API est de ${Math.round(interaction.client.ws.ping)} ms`, ephemeral: true});
   },
 };
