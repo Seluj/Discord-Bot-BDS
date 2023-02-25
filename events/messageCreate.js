417b4c4bf55ce2fbc2fbe11f81aa2e2172d9cdc0
Jules Ferlin <jules.ferlin@gmail.com>
2023-02-25 19:51:27 +0100

    Changed event to handle deletion of undesired message in attribution_sports channel

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,18 +3,28 @@ const { Events } = require('discord.js');
 module.exports = {
   name: Events.MessageCreate,
   async execute(message) {
-    // Ignore all human messages
-    if (!message.author.bot) return;
-
 
     if (message.content.startsWith('bds!')) {
+      // Ignore all human messages
+      if (!message.author.bot) return;
+
       let temp = message.content.split('!')[1];
       let command = message.client.commands.get(temp);
       try {
         await command.execute(message);
       } catch (error) {
-        console.error(`Error executing}`);
+        console.error(`Error executing command ${command.name}`);
         console.error(error);
+      }
+    } else {
+      // Ignore all bot messages
+      if (message.author.bot) return;
+
+      const { attribution_sports } = require(`../serveur/channels/channels_${message.guild.id}.json`);
+
+      // Ignore all messages not in the attribution_sports channel
+      if (message.channelId === attribution_sports) {
+        message.delete();
       }
     }
   },
